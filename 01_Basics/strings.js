5578213faf058e73af83b18b58600f074846e8fd
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-24 18:52:14 +0000

    strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,32 @@
+const name="radha"
+const Repocount=50
+
+console.log(`Hello my name is ${name} and my repo count is${Repocount}`);
+const gamename=new String('Radzz')
+console.log(gamename.length);
+console.log(gamename.toLowerCase());
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('r'));
+
+const newString =gamename.substring(0,2);
+console.log(newString);
+
+const anotherString=gamename.slice(1,5);
+console.log(anotherString);
+
+const nextString="  Pavitra  "
+console.log(nextString.trim());
+
+const url="http://radhika.com/radhika%5deokate"
+console.log(url.replace('5%'));
+console.log(url.includes('chaman'));
+
+console.log(gamename.split('-'));
+
+
+
+
+
+
+
