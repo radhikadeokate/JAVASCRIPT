ee9a34209068d385e9cfa4afd8e50d0beac67663
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-24 16:56:39 +0000

    comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,19 @@
+console.log(2>3);
+console.log(2==3);
+console.log(2>=3);
+console.log(2<3);
+console.log(2!=3);
+
+console.log("2">0);
+console.log(0<"0");
+
+
+console.log(null>0);
+console.log(null==0);
+
+//Dt should be same 
+//=== >strict check 
+
+
+
+
