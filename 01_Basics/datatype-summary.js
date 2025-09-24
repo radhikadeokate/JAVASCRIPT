a93ff64bf86e57a977a9417870f81866c5a7a1e1
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-24 18:04:39 +0000

    stack heap

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -5,3 +5,20 @@
 
 //Reference(non primitive)
 //Arrays ,objects,Functions
+
+//stack(primitive)-copy,heap(non primitive)-Reference(changes in orignal value )
+let myname="radhikadotcom"
+anothername=myname
+anothername="bhaktidotcom"
+console.log(myname);
+console.log(anothername);
+
+let user1 = {
+    email:"radhika@gmail.com",
+    upi :"upi@abl"
+}
+let user2=user1
+user2.email="bhakti@gmail.com"
+console.log(user1);
+console.log(user2);
+
