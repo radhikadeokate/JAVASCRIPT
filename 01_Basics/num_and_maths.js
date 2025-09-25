acfe89630b59005843973e80ce791898f4266799
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-25 07:29:03 +0000

    math and num

---
+++ 01_Basics/num_and_maths.js
@@ -0,0 +1,48 @@
+const balance =new Number(400);
+console.log(balance) ;
+console.log(balance.toFixed(3));
+console.log(balance.toPrecision(5));
+console.log(balance.toString());
+
+const otherNumber=1000000
+console.log(otherNumber.toPrecision(2));
+console.log(otherNumber.toLocaleString('en-IN'));
+
+const score=[2,5,6]
+console.log(score);
+console.log(score.reverse());
+console.log(score.length);
+
+/*****************MATHS******************* */
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.6));
+console.log(Math.ceil(1,3));
+console.log(Math.floor(5,6));
+console.log(Math.pow(2,6));
+console.log(Math.max(23,52,84,59));
+console.log(Math.min(23,52,84,59));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=30;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
