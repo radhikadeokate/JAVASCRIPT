4e7bfbebfbbd2a42194d1f2e11886e42a2ca44e3
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-27 13:38:35 +0000

    arrows

--- 03_Basics/arrows.js
+++ 03_Basics/arrows.js
@@ -1,21 +1,21 @@
 const user={
-    username:"Rd",
-    price:999,
-    welcomeMessage:function(){
-       // console.log(`${this.username},Welcome to Website`)
-//console.log(this);
+    username:"Rd",
+    price:999,
+    welcomeMessage:function(){
+      console.log(`${this.username},Welcome to Website`)
+console.log(this);
 
  }
 }
-// user.welcomeMessage()
-// user.username="PP"
-// user.welcomeMessage()
-//console.log(this)
+user.welcomeMessage()
+user.username="PP"
+user.welcomeMessage()
+console.log(this)
 
-// function chai(){
-//     let usrname ="Radhika"
-//     console.log(this)
-// }
+function chai(){
+    let usrname ="Radhika"
+    console.log(this)
+}
 // chai()
 
 const chai=()=>{
@@ -33,6 +33,6 @@ const chai=()=>{
  //Explicit Return- curly braces used use return
 
  // Implicit Return-No Return 
- 
+
  const addtwo =(num3,num4)=> num3+num4
 console.log(addtwo(5,9));
