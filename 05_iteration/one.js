2fbe7e9355e67ac8d09606ce5d88287a8841fde8
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-27 19:02:30 +0000

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,20 @@
+//For loop
+ for (let i = 1; i < 10; i++) {
+//     const element = i;
+     if(element==5){
+//         console.log("5 is best number");
+         break;
+//     }
+//     console.log(element)  
+// }
+
+// for (let i = 1; i < 50 ; i++) {
+//    // console.log(`Outer loop value:${i}`);
+    
+//     for (let j = 1; j <10 ; j++) {
+//         //console.log(`Inner loop value:${j}and outer loop ${i}`);
+//         //console.log(i+'*'+j+'='+i*j );
+        
+//     }
+    
+//   }
