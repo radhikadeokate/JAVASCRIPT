6e0a397b79e6b85c46ddf36b5d5e33d1dfe62a42
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-27 18:08:28 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+//Switch Syntax 
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+const month ="March"
+switch (month) {
+    case "January":
+        console.log("January");
+         break;
+    case 2:
+        console.log("Feb");
+         break;
+    case "March":
+        console.log("March");
+         break;
+    case 4:
+        console.log("April");
+         break;
+
+    default:
+        console.log("Default case match ");
+        break;
+}
