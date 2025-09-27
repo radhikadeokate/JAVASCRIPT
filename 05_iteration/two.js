3396ab1a866be48dc81437be2064ae5b8e229cd9
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-27 19:15:27 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+//While loop
+let i=0
+while (i<=10) {
+    //console.log(`Value of i is :${i}`)
+    i=i+2
+}
+
+let myArray=['flash',"Batman","Superman"]
+let arr=0
+while (arr<myArray.length) {
+    //console.log(`Value is ${arr}`);
+   arr=arr+1; 
+    
+}
+let score=11;
+do{
+    console.log(`Score is : ${score}`);
+    score++
+}while (score<=10);
