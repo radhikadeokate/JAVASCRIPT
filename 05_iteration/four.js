58691a627dd884963b37726fe5ab5161dee6ac63
Radhika Deokate <radhikadeokate9898@gmail.com>
2025-09-28 08:56:01 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,14 @@
+const myObject={
+    js:"Javascript",
+    cpp:"C++",
+    rb:"Ruby",
+    swift:"swift by apple"
+}
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const myarr=[11,223,43,54,65]
+for (const key in myarr) {
+    console.log(myarr[key]); 
+}
